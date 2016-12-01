f98d4b0c01566e5f6875d4a5e0024b6e077b248a
dwitmexico <jcamprubi@dwit.cat>
2016-12-01 13:58:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var app = express();
+//var express = require('express');
+//var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+//app.set('port', (process.env.PORT || 5000));
 
 //app.use(express.static(__dirname + '/public'));
 
@@ -9,13 +9,13 @@ app.set('port', (process.env.PORT || 5000));
 //app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+//app.get('/', function(request, response) {
+//  response.render('pages/index');
+//});
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+//app.listen(app.get('port'), function() {
+  console.log("test jordi");
+//});
 
 //var server = require("./server3");
 //server.iniciar();
